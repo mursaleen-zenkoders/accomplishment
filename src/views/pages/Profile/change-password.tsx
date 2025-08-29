aa3ac6492c1e2f3913076e3039d0698948d8e568
mursaleen-zenkoders <mursaleen@zenkoders.com>
2025-08-29 21:52:39 +0500

    `Feat`: - Add change password button on profile change password view

--- src/views/pages/Profile/change-password.tsx
+++ src/views/pages/Profile/change-password.tsx
@@ -1,5 +1,6 @@
 import Box from '@/components/common/box';
 import Heading from '@/components/common/heading';
+import { Button } from '@/components/ui/button';
 import { Input } from '@/components/ui/input';
 import { ChangePasswordSchema } from '@/schemas/change-password.schema';
 import { useFormik } from 'formik';
@@ -15,44 +16,50 @@ const ChangePassword = () => {
   });
 
   return (
-    <Box>
-      <Heading text="Change Password" width="medium" size="31" />
+    <div className="space-y-6 flex flex-col items-end">
+      <Box>
+        <Heading text="Change Password" width="medium" size="31" />
 
-      <form onSubmit={handleSubmit} className="flex flex-col gap-y-6">
-        <Input
-          error={touched.oldPassword ? errors.oldPassword : undefined}
-          className="bg-neutral-grey-0 border-none h-14 placeholder:text-neutral-grey-50"
-          placeholder="Enter Old Password"
-          value={values['oldPassword']}
-          onChange={handleChange}
-          label="Old Password"
-          name="oldPassword"
-          type="password"
-        />
+        <form onSubmit={handleSubmit} className="flex flex-col gap-y-6">
+          <Input
+            error={touched.oldPassword ? errors.oldPassword : undefined}
+            className="bg-neutral-grey-0 border-none h-14 placeholder:text-neutral-grey-50"
+            placeholder="Enter Old Password"
+            value={values['oldPassword']}
+            onChange={handleChange}
+            label="Old Password"
+            name="oldPassword"
+            type="password"
+          />
 
-        <Input
-          error={touched.newPassword ? errors.newPassword : undefined}
-          className="bg-neutral-grey-0 border-none h-14 placeholder:text-neutral-grey-50"
-          placeholder="Enter New Password"
-          value={values['newPassword']}
-          onChange={handleChange}
-          label="New Password"
-          name="newPassword"
-          type="password"
-        />
+          <Input
+            error={touched.newPassword ? errors.newPassword : undefined}
+            className="bg-neutral-grey-0 border-none h-14 placeholder:text-neutral-grey-50"
+            placeholder="Enter New Password"
+            value={values['newPassword']}
+            onChange={handleChange}
+            label="New Password"
+            name="newPassword"
+            type="password"
+          />
 
-        <Input
-          error={touched.confirmPassword ? errors.confirmPassword : undefined}
-          className="bg-neutral-grey-0 border-none h-14 placeholder:text-neutral-grey-50"
-          placeholder="Enter Confirm New Password"
-          value={values['confirmPassword']}
-          label="Confirm New Password"
-          onChange={handleChange}
-          name="confirmPassword"
-          type="password"
-        />
-      </form>
-    </Box>
+          <Input
+            error={touched.confirmPassword ? errors.confirmPassword : undefined}
+            className="bg-neutral-grey-0 border-none h-14 placeholder:text-neutral-grey-50"
+            placeholder="Enter Confirm New Password"
+            value={values['confirmPassword']}
+            label="Confirm New Password"
+            onChange={handleChange}
+            name="confirmPassword"
+            type="password"
+          />
+        </form>
+      </Box>
+
+      <Button type="submit" onClick={() => handleSubmit()} className="self-end">
+        Change Password
+      </Button>
+    </div>
   );
 };
 
