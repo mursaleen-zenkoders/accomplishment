df39a214d62ae8da16cc50ad2218dbbbab77d9ba
mursaleen-zenkoders <mursaleen@zenkoders.com>
2025-09-05 19:53:32 +0500

    `Update`: - base api url

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,7 @@ import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
   env: {
+    API_URL: process.env.API_URL,
     SUPABASE_URL: process.env.SUPABASE_URL,
     SUPABASE_ANON_KEY: process.env.SUPABASE_ANON_KEY,
   },
--- src/services/base-url.ts
+++ src/services/base-url.ts
@@ -1,12 +1,12 @@
 const localhost = 'http://localhost:3000/api';
-const live = 'http://192.168.0.253:3000/api';
+const live = process.env.API_URL;
 
 const isLive = true;
 const baseUrl = isLive ? live : localhost;
 
 export const URLS = {
   // =========================== Auth ===================== //
-  FORGET_PASSWORD: `${baseUrl}/auth/forget-password`,
+  FORGET_PASSWORD: `${baseUrl}/auth/forgot-password`,
   RESET_PASSWORD: `${baseUrl}/auth/reset-password`,
   VERIFY_OTP: `${baseUrl}/auth/verify-otp`,
   RESEND_OTP: `${baseUrl}/auth/resend-otp`,
